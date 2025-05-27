20206ecd08c8c27d6ef4b8f1b883e4946c5a3540
Trinityue <trinity.shwdtk@gmail.com>
2025-05-27 17:26:44 +0200

    Alpha V.0.0.1

---
+++ Code/SpawnPoint.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class SpawnPoint : Node2D
+{
+
+    [Export] PackedScene enemyScene;
+    [Export] Node2D[] spawn_Points;
+    [Export] float eps = 1f;
+
+    float spawn_rate;
+    float tus = 0; // time until spawn
+
+    public override void _Ready()
+    {
+        spawn_rate = 1 / eps;
+
+    }
+
+    public override void _Process(double delta)
+    {
+        if (tus > spawn_rate)
+        {
+            Spawn();
+            tus = 0;
+        }
+        else
+        {
+            tus += (float)delta;
+        }
+    }
+
+    private void Spawn()
+    {
+        Vector2 location;
+        Enemy enemy = (enemy)enemyScene.Instantiate();
+        enemy.GlobalPosition = location;
+        GetTree().Root.AddChild(enemy);
+        }
+
+}
