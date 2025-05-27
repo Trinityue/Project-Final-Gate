97ed32c3b44ad50eeca37a9271dd8ec3f037cf81
Trinityue <trinity.shwdtk@gmail.com>
2025-05-27 18:43:55 +0200

    Alpha V.0.0.5

--- Code/EnemySpawner.cs
+++ Code/EnemySpawner.cs
@@ -1,42 +1,8 @@
 using Godot;
 using System;
 
-public partial class SpawnPoint : Node2D
+public partial class Enemy2 : Node2D
 {
 
-    [Export] PackedScene enemyScene;
-    [Export] Node2D[] spawn_Points;
-    [Export] float eps = 1f;
-
-    float spawn_rate;
-    float tus = 0; // time until spawn
-
-    public override void _Ready()
-    {
-        spawn_rate = 1 / eps;
-
-    }
-
-    public override void _Process(double delta)
-    {
-        if (tus > spawn_rate)
-        {
-            Spawn();
-            tus = 0;
-        }
-        else
-        {
-            tus += (float)delta;
-        }
-    }
-
-    private void Spawn()
-    {   RandomNumberGenerator rng = new RandomNumberGenerator();
-        Vector2 location = spawn_Points[rng.Randi() % spawn_Points.Length].GlobalPosition;
-        // Ensure the spawn point is not occupied
-        Enemy enemy = (Enemy)enemyScene.Instantiate();
-        enemy.GlobalPosition = location;
-        GetTree().Root.AddChild(enemy);
-        }
 
 }
