b1d13c46bd77a3e3496460ef76519e11e81379a1
Trinityue <trinity.shwdtk@gmail.com>
2025-06-17 21:02:53 +0200

    Alpha Patch Test V.0.1.4.5

--- Code/MoneyLabel.cs
+++ Code/MoneyLabel.cs
@@ -3,18 +3,5 @@ using System;
 
 public partial class MoneyLabel : Label
 {
-    private GameManager gameManager;
-
-    public override void _Ready()
-    {
-        gameManager = GetNode<GameManager>("/root/Node2D/GameManager");
-    }
-
-    public override void _Process(double delta)
-    {
-        if (gameManager != null)
-        {
-            Text = gameManager.Player_Health.ToString();
-        }
-    }
+ 
 }
