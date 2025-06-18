a8b3965d37a1966d00b5b545c02a264346dd07ca
Trinityue <trinity.shwdtk@gmail.com>
2025-06-18 15:44:53 +0200

    Alpha V.0.1.4.6.1

---
+++ Code/GameLabel.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class GameLabel : Label
+{
+    private GameManager gameManager;
+
+    public override void _Ready()
+    {
+        gameManager = GetNode<GameManager>("/root/Node2D/GameManager");
+    }
+
+    public override void _Process(double delta)
+    {
+        if (gameManager != null)
+        {
+            Text = gameManager.Player_Health.ToString();
+        }
+    }
+}
