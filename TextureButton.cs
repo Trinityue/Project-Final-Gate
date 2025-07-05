d91ddf3e66a8af8418103cb7cb5a2e6c3de38d82
CartiPlayboi33 <janosch_kr@hotmail.com>
2025-07-05 16:00:38 +0200

    CartiPlayboi33

--- TextureButton.cs
+++ TextureButton.cs
@@ -1,10 +1,13 @@
 using Godot;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 public partial class TextureButton : Godot.TextureButton
 {
     public void _on_TextureButton_pressed()
-    {
+    { 
+        var AudioStreamPlayer2D = GetNode<Buttonclicksound>("Buttonclicksound");  
+        AudioStreamPlayer2D.Play();
         GetTree().ChangeSceneToFile("res://node_2d.tscn");
     }
 }
